59ec5ff88f79284d85dce831c93cbcbd11d2c8a5
olga <diakonovao@gmail.com>
2020-06-15 15:53:02 -0400

    first commit with Springboot; landing page with GET request

---
+++ src/main/java/com/go2it/edu/springboot/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.go2it.edu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
