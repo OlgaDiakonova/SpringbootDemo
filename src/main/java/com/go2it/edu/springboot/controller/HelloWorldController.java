d862723a3ab5c9e34d1a282b765843afb8a75d32
olga <diakonovao@gmail.com>
2020-06-15 16:52:08 -0400

    adding pages with POST request

--- src/main/java/com/go2it/edu/springboot/controller/HelloWorldController.java
+++ src/main/java/com/go2it/edu/springboot/controller/HelloWorldController.java
@@ -1,7 +1,8 @@
 package com.go2it.edu.springboot.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.openjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
@@ -14,6 +15,41 @@ public class HelloWorldController {
 
                 "<img src=https://cdn-images-1.medium.com/fit/t/1600/672/0*n-2bW82Z6m6U2bij.jpeg></img>"
 
+                + "</div>";
+
+    }
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String getHelloWorldMessageForUser(@RequestParam String name, String surname) {
+        return "<div style=\"text-align:center;\">" + "<h1>Welcome, " + name + " " + surname + "</h1>"
+
+                + "<p> This is my first web-page </p>" + "</div>";
+
+    }
+
+    @RequestMapping(value = "/{resource}", method = RequestMethod.GET)
+    public String getHelloWorldMessageFromResource(@PathVariable String resource) {
+        return "<div style=\"text-align:center;\">" + "<h1>This request was done to the resource: " + resource
+                + "</h1>" + "</div>";
+
+    }
+    @RequestMapping(value = "/customizedWelcome", method = RequestMethod.POST)
+    public String getHelloWorldWithFromJSON(@RequestBody String message) {
+        JSONObject json = new JSONObject(message);
+        String firstName = "Albert";
+        String defaultPictureAddress = "https://cdn-images-1.medium.com/fit/t/1600/672/0*n-2bW82Z6m6U2bij.jpeg";
+        String firstNameFromRequest = json.getString("firstName");
+        if (StringUtils.isNotBlank(firstNameFromRequest)) {
+            firstName = firstNameFromRequest;
+
+        }
+        String pictureURLFromRequest = json.getString("pictureURL");
+        if (StringUtils.isNotBlank(pictureURLFromRequest)) {
+            defaultPictureAddress = pictureURLFromRequest;
+
+        }
+        return "<div style=\"text-align:center;\">" + "<h1>Hello world from " + firstName + "</h1>"
+
+                + "<p> This is my first web-page </p>" + "<img src=" + defaultPictureAddress + "></img>"
                 + "</div>";
 
     }
